3697086dfb4b6d3c4fd3c703066222175e0b8914
Anurati2004 <dasanurati4@gmail.com>
2026-01-17 17:22:04 +0530

    Updated API

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -10,7 +10,7 @@ export default function Quiz() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/api/questions")
+    fetch("https://quiz-backend-dofg.onrender.com")
       .then(res => res.json())
       .then(data => setQuestions(data))
       .catch(err => console.error("Failed to fetch questions:", err));
